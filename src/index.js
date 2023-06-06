73d9feef1a3bcea062aa57d38609809d02bf86f0
Selaine Rodriguez <selainer17@yahoo.com>
2023-06-06 02:38:17 -0700

    Adjust index to try and fix routing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
+);
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
