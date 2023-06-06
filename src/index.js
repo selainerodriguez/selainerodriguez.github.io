7196c8b1e8cfcce2bcf20e27def0f55f64f00961
Selaine Rodriguez <selainer17@yahoo.com>
2023-06-06 03:07:30 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,10 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-        </Route>
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
