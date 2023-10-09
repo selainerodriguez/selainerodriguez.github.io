365a725ea61d8abd622e05930ce9437e89728f08
Selaine Rodriguez <selainer17@yahoo.com>
2023-10-08 23:12:34 -0700

    Check for changes in app and updates

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code>.
+          Selaine's React App Starter
         </p>
         <a
           className="App-link"
